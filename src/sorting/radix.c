b0ab1b2590de243ef1cf0fd18830bbfa7ce9c0a5
JuanmaMC92 <juanmam0@hotmail.com>
2025-01-26 09:27:22 +0100

    Radix Ok, 1084 for 100 elements

--- src/sorting/radix.c
+++ src/sorting/radix.c
@@ -16,28 +16,36 @@ void proc_radix(t_stack *a,t_stack*b,t_log *log)
 {
     
     int mask;
-    int bit_count;
-    int bit;
+    
     t_node *current;
     
     mask=1;
 
+    int bit;
+    int num;
+    int size;
+    int bit_count;
 
-    bit_count=get_max_bits(a);
-    bit=1;
-    while(bit <= bit_count)
+    size=a->size;
+    num=size-1; 
+    bit_count=0;
+    while((num>>bit_count)!=0)
+        bit_count++;
+    //bit_count=get_max_bits(a);
+    bit=0;
+    while(bit < bit_count)
     {
-        while(stackrem(a,bit,!mask))
+        //while(stackrem(a,bit,!mask))
+        size=a->size;
+        while(size--)
         {
             current=a->top;
             if(get_bit(current->index,bit)== mask)
             {
                 ra(a,b,log);
-                /*
-                current=b->top;
-                if(current->next && current->index > current->next->index)
-                    sb(a,b,log);
-                */
+                //current=b->top;
+                //if(current->next && current->index > current->next->index)
+                //  sb(a,b,log);
             } 
             else
                 pb(a,b,log);
@@ -47,19 +55,13 @@ void proc_radix(t_stack *a,t_stack*b,t_log *log)
         ft_printf("%d bit:\n",bit);
         ft_printf("A to B\n");
         print_stacks(a,b);
-
         while(b->size>0)
         {
             pa(a,b,log);
-            /*
-            current=a->top;
-            if(current->next && current->index > current->next->index)
-                sa(a,b,log);
-            */
+            //current=a->top;
+            //if(current->next && current->index > current->next->index)
+            //    sa(a,b,log);
         }
         bit++;
-        if(is_sorted(a))
-            break;
     }
-
 }
